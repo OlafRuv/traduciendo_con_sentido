30112b6fb2ce9a2ea75cd50ec1143a1fe56130c7
Olaf <olaf.ruvag@gmail.com>
2022-03-14 21:42:58 -0600

    Scroll section con nuevo estilo y widgets personalizados

--- lib/src/pages/scroll_section/scroll_page.dart
+++ lib/src/pages/scroll_section/scroll_page.dart
@@ -1,197 +1,211 @@
 import 'package:flutter/material.dart';
+import 'package:tcs/widgets/custom_background.dart';
+import 'package:tcs/widgets/widgets.dart';
 
-
+// Widget de sliders previos al ingreso a la aplicacion 
 class ScrollPage extends StatelessWidget {
   const ScrollPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: PageView( //PAGEVIEW DONDE SE HACE EL SCROLL
+      body: PageView( // En total son 3 paginas y cada una se crea en su propia funcion
         children: [
-          _pagina1(),
-          _pagina2(),
+          _pagina1(context),
+          _pagina2(context),
           _pagina3(context)
         ],
       )
     );
   }
 
-
-  //COLOR DE FONDO EXTERIOR
-  Widget _colorFondoExterior(){
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      color: Colors.green[400],
-    );
-  }
-
-
   //PAGINA 1
-  Widget _pagina1() {
-    return Stack(
+  Widget _pagina1(BuildContext context){
+    return Stack( // Un stack que almacena nuestro custom background que usa un logo como titulo y que al mismo tiempo recibe el texto que despiega la pagina
       children: [
-        _colorFondoExterior(),
-        _contenidoPagina1(),
+        const CustomBackground(
+          title: Image(image: AssetImage('assets/TraduciendoConSentido.png'),height:280.0)),
+        _contenidoPagina1v2(
+          context: context, 
+          texto: 'La misión y visión de TCS es permitir al público general y a personas con alguna discapacidad visual traducir lenguaje convencional a lenguaje braille, aplicandolo de una manera simple y accesible. Ademas, se pretende facilitar que las personas con discapacidades visuales puedan tener acceso a información escrita, tal y como lo gozan las personas sin estas condiciones.',
+          ),
       ],
     );
   }
+  // 'La misión y visión de TCS es permitir al público general y a personas con alguna discapacidad visual traducir lenguaje convencional a lenguaje braille, aplicandolo de una manera simple y accesible. Ademas, se pretende facilitar que las personas con discapacidades visuales puedan tener acceso a información escrita, tal y como lo gozan las personas sin estas condiciones.'
 
-  /* PONER ESTE CODIGO EN CUANTO SE ENCUENTRE ALGUNA FOTO BUENA PARA EL FONDO
-  Widget _imagenFondo(){
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Image(
-        image: AssetImage('assets/scroll-1.png'),
-        fit: BoxFit.cover// sive para que pueda llenar los espacios que no se pudieron llenar
-      )
-    );
-  }
-  */
-
-  //CONTENIDO DEL CUERPO DE LA PAGINA 1
-  Widget _contenidoPagina1(){
-    final estiloTexto = TextStyle(color: Colors.white, fontSize: 20.0); //estilo para el texto 
-    return SafeArea(
-      child: Center(
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-          padding: EdgeInsets.all(20.0),
-          //color: Colors.green[800],
-          decoration: BoxDecoration(
-            color: Colors.green[800],
-            borderRadius: BorderRadius.circular(20.0)
-          ),
-          child: Column(
-            children: [
-              
-              Expanded(child: Container()),
-              Text('Bienvenido a Traduciendo Con Sentido.', style: estiloTexto, textAlign: TextAlign.center,),
-              SizedBox(height: 20.0),
-              Text('La misión y visión de TCS es permitir al público general y a personas con alguna discapacidad visual traducir lenguaje convencional a lenguaje braille, aplicandolo de una manera simple y accesible. Ademas, se pretende facilitar que las personas con discapacidades visuales puedan tener acceso a información escrita, tal y como lo gozan las personas sin estas condiciones.', style: estiloTexto, textAlign: TextAlign.center,),
-              Expanded(child: Container()),
-              
-              Text('Desliza a la derecha', style: estiloTexto,),
-
-              Icon(Icons.keyboard_arrow_right, size: 70.0, color: Colors.white,)
-            ],
-          ),
+  Widget _pagina2(BuildContext context) {
+    return Stack(// Un stack que almacena nuestro custom background que usa un logo como titulo y que al mismo tiempo recibe el texto que despiega la pagina
+      children: [
+        const CustomBackground(
+          reversed: true,
+          title: Image(image: AssetImage('assets/TraduciendoConSentido.png'),height:280.0)
         ),
-      ),
-    );
-  }
-
-
-
-  //PAGINA 2
-  Widget _pagina2() {
-    return Stack(
-      children: [
-        _colorFondoExterior(),
-        _contenidoPagina2(),
+        _contenidoPagina1v2(
+          context: context, 
+          texto: 'En esta aplicación podras traducir texto convencional a braille, procesar texto de documentos para su traducción al braille y procesar texto de imágenes para su traducción al braille. Al registrarse tambien podra hacer uso del acceso de sus documentos en linea',
+          extra: const Image(
+            height: 100,
+            image: AssetImage('assets/braille.png')
+          )
+        ),
       ],
     );
   }
+  // En esta aplicación podras traducir texto convencional a braille, procesar texto de documentos para su traducción al braille y procesar texto de imágenes para su traducción al braille. Al registrarse tambien podra hacer uso del acceso de sus documentos en linea
 
-
-  //CONTENIDO DEL CUERPO DE LA PAGINA 2
-  Widget _contenidoPagina2(){
-    final estiloTexto = TextStyle(color: Colors.white, fontSize: 20.0); //estilo para el texto 
-    return SafeArea(
-      child: Center(
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-          padding: EdgeInsets.all(20.0),
-          decoration: BoxDecoration(
-              color: Colors.green[800],
-              borderRadius: BorderRadius.circular(20.0)
+  Widget _pagina3(BuildContext context) {
+    return Stack(// Un stack que almacena nuestro custom background que usa un logo como titulo y que al mismo tiempo llama a la funcion que contruye los botnes y la seccion de esta pagina
+      children: [
+        const CustomBackground(
+          title: Image(image: AssetImage('assets/TraduciendoConSentido.png'),height:400.0)
+        ),
+        _contenidoPagina3(context:context),
+      ],
+    );
+  }
+ 
+  // Esta es la composicion de las primeras 2 paginas 
+  Widget _contenidoPagina1v2({
+    required BuildContext context, 
+    required String texto, 
+    Widget? extra
+    }){
+    final size = MediaQuery.of(context).size;
+    const estiloTexto = TextStyle(
+      color: Colors.black, 
+      fontSize: 20.0
+    ); //estilo para el texto 
+    return SingleChildScrollView( //ME VA A PERMITIR HACER SCROLL DEPENDIENDO DEL TAMAÑO DEL HIJO
+      child: Column(
+        children: [
+          SafeArea(
+            child: Container(
+              height: 150.0,
+            ),
           ),
-          child: Center(
+          Container( // ESTO ES PURO ESTILO DE LAS TAJETAS DE INFORMACUON
+            width: size.width * 0.85,
+            margin: const EdgeInsets.symmetric(vertical: 30.0),
+            alignment:  Alignment.center,          
+            padding: const EdgeInsets.only(
+              bottom: 10.0,
+              top: 30.0,
+              left: 20.0,
+              right: 20.0,
+              ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30.0),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0, 5.0),
+                  spreadRadius: 3.0,
+                )
+              ]
+            ),
             child: Column(
-              children: [
-                Expanded(child: Container()),
-                Text('En esta aplicación podras traducir texto convencional a braille, procesar texto de documentos para su traducción al braille y procesar texto de imágenes para su traducción al braille. Al registrarse tambien podra hacer uso del acceso de sus documentos en linea', style: estiloTexto, textAlign: TextAlign.center),
-                SizedBox(height: 50.0,),
-                Icon(Icons.face_retouching_natural, size: 100.0, color: Colors.white,),
-                Expanded(child: Container()),
-                Text('Desliza a la derecha', style: estiloTexto,),
-                Icon(Icons.keyboard_arrow_right, size: 70.0, color: Colors.white,)
+              children:  [ // ESTE TEXTO Y EL WIDGET EXTRA SON LOS ELEMENTOS DENTRO DE LA TARJETA DE INFORMACION 
+                Text(
+                  texto, 
+                  style: estiloTexto, 
+                  textAlign: TextAlign.center,
+                ),
+                if (extra == null) ... [
+                   const SizedBox(height: 20.0,),
+                ]
+                else ... [
+                  extra,
+                  const SizedBox(height: 20.0,),
+                ]
+                ,
+                const Text('Desliza a la izquierda', style: estiloTexto,), // GIF PARA MOSTRAR HACIA DONDE DESLIZAR
+                const Image(
+                  image: AssetImage('assets/Swipe_Left.gif'),
+                  height:100.0
+                ),
               ],
             ),
           ),
-        ),
+        ],
+      ),
+    );
+  }
+  Widget _contenidoPagina3({
+    required BuildContext context,
+    }){
+    final size = MediaQuery.of(context).size;//estilo para el texto 
+    return SingleChildScrollView( //ME VA A PERMITIR HACER SCROLL DEPENDIENDO DEL TAMAÑO DEL HIJO
+      child: Column(
+        children: [
+          SafeArea(
+            child: Container(
+              height: 280.0,
+            ),
+          ),
+          Container(  // ESTO ES PURO ESTILO DE LAS TAJETAS DE INFORMACUON
+            width: size.width * 0.9,
+            margin: const EdgeInsets.symmetric(vertical: 30.0),
+            alignment:  Alignment.center,          
+            padding: const EdgeInsets.only(
+              bottom: 30.0,
+              top: 30.0,
+              left: 20.0,
+              right: 20.0,
+              ),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(30.0),
+              boxShadow: const [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0, 5.0),
+                  spreadRadius: 3.0,
+                )
+              ]
+            ),
+            child: Column(
+              children:  [  // Lat tarjeta tiene 2 botnes, el de INICIAR SESION y el de CREAR SESION, estos se construyen en base al widget previamente definido de Custom Button
+                const SizedBox(height: 20),
+                CustomButton(
+                  buttontext: 'Iniciar Sesión', 
+                  padHButton: 40, 
+                  padVButton: 30,
+                  elevationButton: 20,
+                  iconData: Icons.supervised_user_circle_rounded,
+                  iconSize: 30,
+                  textSize: 24,
+                  onPressedFunction: (){
+                    Navigator.pushNamed(context, 'iniciar_sesion');
+                  }, 
+                ),
+                // estiloBotones(context, Icons.account_box, 'Iniciar sesión', 'iniciar_sesion'),
+                const SizedBox(height: 30),
+                const Divider(height: 20, thickness: 3, indent: 20, endIndent: 20),
+                const SizedBox(height:30),
+                CustomButton(
+                  buttontext: 'Crear Sesión', 
+                  padHButton: 40, 
+                  padVButton: 30,
+                  elevationButton: 20,
+                  iconData: Icons.create,
+                  iconSize: 30,
+                  textSize: 24,
+                  onPressedFunction: (){
+                    Navigator.pushNamed(context, 'crear_sesion');
+                  }, 
+                ),
+                const SizedBox(height:30),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-
-
-  //PAGINA 3
-  Widget _pagina3(BuildContext context) {
-    return Stack(
-      children: [
-        _colorFondoExterior(),
-        _contenidoPagina3(context),
-      ],
-    );
-  }
-
-  
-  
-  //CONTENIDO DEL CUERPO DE LA PAGINA 3
-  Widget _contenidoPagina3(BuildContext context){
-    return SafeArea(
-      child: Container(
-        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-        padding: EdgeInsets.all(20.0),
-        decoration: BoxDecoration(
-            color: Colors.green[800],
-            borderRadius: BorderRadius.circular(20.0)
-        ),
-        child: Center(
-          child: Column(
-            children: [
-              Image(image: AssetImage('assets/tcs_logo.png'), height: 200.0, width: 300.0),
-              Expanded(child: Container()),
-              estiloBotones(context, Icons.account_box, 'Iniciar sesión', 'iniciar_sesion'),
-              Expanded(child: Container()),
-              Divider(height: 20, thickness: 5, indent: 20, endIndent: 20),
-              Expanded(child: Container()),
-              estiloBotones(context, Icons.create, 'Crear sesión', 'crear_sesion'),
-              Expanded(child: Container()),
-            ],
-          )
-        ),
-      )
-    );
-  }
-
-  //DAR ESTILO A LOS BOTONES
-  Widget estiloBotones(BuildContext context, IconData icono, String texto, String ruta){
-    return MaterialButton(
-      height: 40.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0), 
-                ),
-                color: Colors.white,
-                textColor: Colors.black87,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(icono, size: 50.0),
-                    SizedBox(width: 30.0,),
-                    Padding(
-                      padding: EdgeInsets.symmetric(vertical: 20.0),                    
-                      child: Text(texto, style: TextStyle(fontSize: 25.0),),
-                    ),
-                  ],
-                ),
-                onPressed: (){
-                  Navigator.pushNamed(context, ruta);
-                },
-      );
-  }
 
 }
--- lib/src/pages/scroll_section/scroll_section.dart
+++ lib/src/pages/scroll_section/scroll_section.dart
@@ -1,1 +1,2 @@
 export 'package:tcs/src/pages/scroll_section/scroll_page.dart';
+// Exportamos la seccion de scroll
