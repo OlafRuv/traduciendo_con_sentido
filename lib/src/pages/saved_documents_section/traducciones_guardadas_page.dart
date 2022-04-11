587bc582a3e0398299b4897d7226daa3aa5c1e8c
Hector <hector.lomeli5514@alumnos.udg.mx>
2022-04-10 23:51:20 -0500

    se arreglo lo que mostrara la seccion de traduciones guardadas y prueba con descarga pdf

--- lib/src/pages/saved_documents_section/traducciones_guardadas_page.dart
+++ lib/src/pages/saved_documents_section/traducciones_guardadas_page.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:tcs/widgets/widgets.dart';
+import 'package:syncfusion_flutter_pdf/pdf.dart';
 
+import 'guardar_pdf.dart';
 
 class TraduccionesGuardadasPage extends StatefulWidget {
   const TraduccionesGuardadasPage({Key? key}) : super(key: key);
@@ -34,7 +39,6 @@ CollectionReference firebaseFirestore = FirebaseFirestore.instance.collection("u
       ),
       body: Stack(
         children: [
-          // _fondoApp(),
           mostrarTraduccionesGuardadas()
         ] 
       ),
@@ -48,85 +52,113 @@ CollectionReference firebaseFirestore = FirebaseFirestore.instance.collection("u
         stream: _usersStream,
         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
           if (snapshot.hasError) {
-            return const Text('Something went wrong');
+            return const Text('Algo salio mal');
           }
       
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text("Loading");
+            return const Text("Cargando ...");
           }
           
           return ListView(
             children: snapshot.data!.docs.map((DocumentSnapshot document) {
             Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-              return ListTile(
-                //title: Text(data['Nombre_usuario']),
-                title: Text(data['Titulo']),
-                subtitle: Text(data['Texto_guardado']),
-                //trailing: Text(data['Uid']),
-                trailing: MaterialButton(
-                      onPressed: (){
-                        showDialog( 
-                          context: context, 
-                          builder: (BuildContext context) => 
-                          AlertDialog(
-                          title: const Text('¿Está seguro en eliminar esta traducción?'),
-                          actions: [
-                            MaterialButton(
-                              onPressed: () {
-                                  Navigator.pushNamed( context, 'traducciones_guardadas');
-                              },
-                              textColor: Theme.of(context).primaryColor,
-                              child: const Text('Cancelar'),
-                            ),
-                            MaterialButton(
-                              onPressed: () {
-                                print("DOCUMENTO ELIMINDADO: " + document.id);
-                                FirebaseFirestore.instance.collection('usuarios').doc(document.id).delete(); //GRACIAS A QUE ACCEDEMOS MEDIANTE EL ID DEL DOCUMENTO, LA ELIMINACION ES MAS SENCILLA
-                                  Navigator.pushNamed( context, 'traducciones_guardadas');
-                              },
-                              textColor: Theme.of(context).primaryColor,
-                              child: const Text('Eliminar'),
-                            ),
-                          ],
-                        )
-                        );
-                        
-                        /*showDialog( //ALERTA DIALOG QUE NOS SERVIRA PARA ALERTAR AL USUARIO QUE SE GUARDO CON EXITO SU TEXTO
-                          context: context, 
-                          builder: (BuildContext context) => popUpEliminarRegistro(context)
-                        );*/
-                        //=============================================
-                        //print("DOCUMENTO ELIMINDADO: " + document.id);
-                        //FirebaseFirestore.instance.collection('usuarios').doc(document.id).delete(); //GRACIAS A QUE ACCEDEMOS MEDIANTE EL ID DEL DOCUMENTO, LA ELIMINACION ES MAS SENCILLA
-                        //===========================================
-                        
-                      },
-                      child: const Text('Eliminar'),
+              return Column(
+                children: <Widget>[
+                  Container(height: 1, color: Colors.green), //ESTE CONTAINER LE DARA ESA LINEA DE SEPARACION A CADA ELEMENTO DE LA LISTA
+                  ListTile(
+                    title: Text(data['Titulo']), //MOSTRARA EL TITULO DE CADA ELEMENTO GUARDADO DE FIRESTORE
+                    subtitle: Text(data['Descripción']), //MOSTRARA LA DESCRIPCION DE CADA ELEMENTO GUARDADO DE FIRESTORE
+                    trailing: Wrap(
+                      children: [ 
+                        MaterialButton(
+                            onPressed: (){
+                              showDialog( 
+                                context: context, 
+                                builder: (BuildContext context) => 
+                                AlertDialog(
+                                title: const Text('Texto de traducción'),
+                                content: Container(
+                                  child: SingleChildScrollView(
+                                    child: Text(data['Texto_guardado'], style: const TextStyle(fontFamily: 'braile_font',fontSize: 20, height: 1.5)), //MOSTRARA EL TEXTO DE NUESTRO ELEMENTO SELECCIONADO DE FIRESTORE
+                                  )
+                                ),
+                                actions: [
+                                  MaterialButton(
+                                    onPressed: () {
+                                      _crearPDF(data['Titulo'], data['Texto_guardado']); //FUNCION QUE TOMARA LOS VALORES A MOSTRAR EN EL PDF (TITULO Y TEXTO)
+                                      Navigator.pushNamed( context, 'traducciones_guardadas');
+                                    },
+                                    textColor: Theme.of(context).primaryColor,
+                                    child: const Text('Descargar PDF'),
+                                  ),
+                                  MaterialButton(
+                                    onPressed: () {
+                                        Navigator.pushNamed( context, 'traducciones_guardadas');
+                                    },
+                                    textColor: Theme.of(context).primaryColor,
+                                    child: const Text('Regresar'),
+                                  ),
+                                ],
+                                
+                              )
+                              );
+                            },
+                            child: const Text('Traducción'),
+                          ),
+                        MaterialButton(
+                            onPressed: (){
+                              showDialog( 
+                                context: context, 
+                                builder: (BuildContext context) => 
+                                AlertDialog(
+                                title: const Text('¿Está seguro en eliminar esta traducción?'),
+                                actions: [
+                                  MaterialButton(
+                                    onPressed: () {
+                                        Navigator.pushNamed( context, 'traducciones_guardadas');
+                                    },
+                                    textColor: Theme.of(context).primaryColor,
+                                    child: const Text('Cancelar'),
+                                  ),
+                                  MaterialButton(
+                                    onPressed: () {
+                                      print("DOCUMENTO ELIMINDADO: " + document.id);
+                                      FirebaseFirestore.instance.collection('usuarios').doc(document.id).delete(); //GRACIAS A QUE ACCEDEMOS MEDIANTE EL ID DEL DOCUMENTO, LA ELIMINACION ES MAS SENCILLA
+                                        Navigator.pushNamed( context, 'traducciones_guardadas');
+                                    },
+                                    textColor: Theme.of(context).primaryColor,
+                                    child: const Text('Eliminar'),
+                                  ),
+                                ],
+                              )
+                              );                            
+                            },
+                            child: const Text('Eliminar'),
+                          ),                      
+                      ]
                     ),
-                    
-                /*onTap: (){
-                  print("DOCUMENTO ELIMINDADO: " + document.id);
-                  FirebaseFirestore.instance.collection('usuarios').doc(document.id).delete(); //GRACIAS A QUE ACCEDEMOS MEDIANTE EL ID DEL DOCUMENTO, LA ELIMINACION ES MAS SENCILLA
-                },*/
-                /*Row(
-                  children: [
-                    MaterialButton(
-                      onPressed: (){},
-                      child: const Text('Imprimir'),
-                    ),
-                    MaterialButton(
-                      onPressed: (){},
-                      child: const Text('Eliminar'),
-                    )
-                  ],
-                ),*/
-
+                  )
+                ]
               );
             }).toList(),
           );
-          
         },
-          );
+        
+    );
+  }
+
+  //APARTADO DONDE SE CREA EL PDF A DESCARGAR
+  Future<void> _crearPDF(String tituloPDF, String textoPdf) async {
+    PdfDocument documentoPDF = PdfDocument();
+    final pagina = documentoPDF.pages.add();
+
+    pagina.graphics.drawString(textoPdf, PdfStandardFont(PdfFontFamily.helvetica, 30));
+    
+
+    List<int> bytes = documentoPDF.save();
+    documentoPDF.dispose();
+
+    guardarMostrarPdf(bytes, tituloPDF);
   }
 
   
