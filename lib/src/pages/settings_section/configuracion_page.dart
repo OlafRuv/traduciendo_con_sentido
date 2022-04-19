7e5aee94a27042230a9415ce49d5fa30fe3d9a56
Olaf <olaf.ruvag@gmail.com>
2022-04-18 22:31:19 -0500

    Implementacion TODO expanded en column

--- lib/src/pages/settings_section/configuracion_page.dart
+++ lib/src/pages/settings_section/configuracion_page.dart
@@ -22,44 +22,42 @@ class _ConfiguracionPageState extends State<ConfiguracionPage> {
         title: const Text('CONFIGURACIÓN'),
       ),
 
-      body: Stack(
+      body: Column(
         children: [
-          SingleChildScrollView( //similar a listview, la diferencia es que abarca toda la pantalla
-              child: Column(
-                children: [
-                  const CustomPageDescription(title: 'Configuración e información de usuario'), // Titulo o descripcion de la pagina
-                  const Text('Usuario:',
-                    style: TextStyle(fontSize: AppTheme.size20),
-                  ),
-                  // TODO: Debemos de sacar la llamada a la base de datos de aquí
-                  Text((usuario == null ? 'Usuario no registrado' : FirebaseAuth.instance.currentUser!.email.toString()), 
-                  style: const TextStyle(fontSize: AppTheme.size20),
-                  ), //si el usuario se encuentra en sesion mostrara el correo del usuario y si esta en null mostrara que el usuario no esta registrado
-                  // Politicas de uso y privacidad que se despliegan haciendo uso del widget de outlined button que creamos
-                  const SizedBox(height: 30.0,),
-                  const CustomOutlinedButton(textContent: 'Políticas de Privacidad',textSize: AppTheme.size20, route: 'politicas_privacidad'),
-                  const SizedBox(height: 30.0,),
-                  const CustomOutlinedButton(textContent: 'Políticas de uso',textSize: AppTheme.size20, route: 'politicas_uso'),
-                  // TODO: Editar para que sea una caja o alguna seccion expandida
-                  const SizedBox(height: 200.0,),
-                  // _botonSalir(context),  
-                  CustomButton( // Para cerrar sesion contamos con uno de nuestros cutom button hasta el fondo de la pagina
-                    padHButton: 80.0,
-                    padVButton: 20.0,
-                    buttontext: 'Cerrar Sesión', 
-                    colorButton: AppTheme.buttonColorv2,
-                    onPressedFunction: () async { 
-                      await FirebaseAuth.instance.signOut(); //metodo para que el usuario salga de la sesion de firebase
-                      setState(() {});
-                      Navigator.pushNamedAndRemoveUntil(
-                        context, 'scroll', 
-                        (route) => false
-                      );
-                    }
-                  ),
-                ],
-              ),
-            )
+          const CustomPageDescription(title: 'Configuración e información de usuario'), // Titulo o descripcion de la pagina
+          const Text('Usuario:',
+            style: TextStyle(fontSize: AppTheme.size20),
+          ),
+          // TODO: Debemos de sacar la llamada a la base de datos de aquí
+          Text((usuario == null ? 'Usuario no registrado' : FirebaseAuth.instance.currentUser!.email.toString()), 
+          style: const TextStyle(fontSize: AppTheme.size20),
+          ), //si el usuario se encuentra en sesion mostrara el correo del usuario y si esta en null mostrara que el usuario no esta registrado
+          // Politicas de uso y privacidad que se despliegan haciendo uso del widget de outlined button que creamos
+          const SizedBox(height: 30.0,),
+          const CustomOutlinedButton(textContent: 'Políticas de Privacidad',textSize: AppTheme.size20, route: 'politicas_privacidad'),
+          const SizedBox(height: 30.0,),
+          const CustomOutlinedButton(textContent: 'Políticas de uso',textSize: AppTheme.size20, route: 'politicas_uso'),
+          Expanded(
+            child: Container(
+              width: 100,
+            ),
+          ),
+          // _botonSalir(context),  
+          CustomButton( // Para cerrar sesion contamos con uno de nuestros cutom button hasta el fondo de la pagina
+            padHButton: 80.0,
+            padVButton: 20.0,
+            buttontext: 'Cerrar Sesión', 
+            colorButton: AppTheme.buttonColorv2,
+            onPressedFunction: () async { 
+              await FirebaseAuth.instance.signOut(); //metodo para que el usuario salga de la sesion de firebase
+              setState(() {});
+              Navigator.pushNamedAndRemoveUntil(
+                context, 'scroll', 
+                (route) => false
+              );
+            }
+          ),
+          const SizedBox(height: 10),
         ],
       ),
       bottomNavigationBar: const CustomBottomNavigation(botonBarraActual: 2),
