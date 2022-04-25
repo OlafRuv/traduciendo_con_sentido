985d2c1bdeb7999675fa3f9a327e70b4f8c79d33
Olaf <olaf.ruvag@gmail.com>
2022-04-24 23:09:44 -0500

    Modularizacion de pagina y uso de tarjetas implementado

--- lib/src/pages/saved_documents_section/traducciones_guardadas_page.dart
+++ lib/src/pages/saved_documents_section/traducciones_guardadas_page.dart
@@ -1,13 +1,8 @@
-// import 'dart:io';
-// import 'dart:typed_data';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tcs/theme/app_theme.dart';
 import 'package:tcs/widgets/widgets.dart';
-import 'package:syncfusion_flutter_pdf/pdf.dart';
-
-import 'guardar_pdf.dart';
 
 class TraduccionesGuardadasPage extends StatefulWidget {
   const TraduccionesGuardadasPage({Key? key}) : super(key: key);
@@ -17,18 +12,13 @@ class TraduccionesGuardadasPage extends StatefulWidget {
 }
 
 class _TraduccionesGuardadasPageState extends State<TraduccionesGuardadasPage> {
-CollectionReference firebaseFirestore = FirebaseFirestore.instance.collection("usuarios");
+  CollectionReference firebaseFirestore = FirebaseFirestore.instance.collection("usuarios");
+  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('usuarios').where('Nombre_usuario',
+   isEqualTo: FirebaseAuth.instance.currentUser!.email.toString()).snapshots();
 
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('usuarios').where('Nombre_usuario', isEqualTo: FirebaseAuth.instance.currentUser!.email.toString()).snapshots();
-
-
-
-  //PARTE DONDE SE CONSULTARA A LA BD----------------------------------------------------------
   @override
   void initState() {
     super.initState();
-    //obtenerUsuarios(); (//SE COMENTO TEMPORALMENTE PARA USO DE PRUEBAS)
-    
   }
 
   @override
@@ -46,152 +36,181 @@ CollectionReference firebaseFirestore = FirebaseFirestore.instance.collection("u
     );
   }
 
-
   Widget mostrarTraduccionesGuardadas(){
     return StreamBuilder<QuerySnapshot>(
-        stream: _usersStream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return const Text('Algo salio mal');
-          }
-      
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Text("Cargando ...");
-          }
-          
-          return ListView(
-            children: snapshot.data!.docs.map((DocumentSnapshot document) {
-            Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
-              return Column(
-                children: <Widget>[
-                  Container(height: 1, color: Colors.green), //ESTE CONTAINER LE DARA ESA LINEA DE SEPARACION A CADA ELEMENTO DE LA LISTA
-                  ListTile(
-                    title: Text(data['Titulo']), //MOSTRARA EL TITULO DE CADA ELEMENTO GUARDADO DE FIRESTORE
-                    subtitle: Text(data['Descripción']), //MOSTRARA LA DESCRIPCION DE CADA ELEMENTO GUARDADO DE FIRESTORE
-                    trailing: Wrap(
-                      children: [ 
-                        MaterialButton(
-                            onPressed: (){
-                              showDialog( 
-                                context: context, 
-                                builder: (BuildContext context) => 
-                                AlertDialog(
-                                title: const Text('Traducción de texto'),
-                                content: SingleChildScrollView(
-                                  child: Text(data['Texto_guardado'], style: const TextStyle(fontFamily: 'braile_font',fontSize: 20, height: 1.5)), //MOSTRARA EL TEXTO DE NUESTRO ELEMENTO SELECCIONADO DE FIRESTORE
-                                ),
-                                actions: [
-                                  MaterialButton(
-                                    onPressed: () {
-                                      _crearPDF(data['Titulo'], data['Texto_guardado']); //FUNCION QUE TOMARA LOS VALORES A MOSTRAR EN EL PDF (TITULO Y TEXTO)
-                                      Navigator.pushNamed( context, 'traducciones_guardadas');
-                                    },
-                                    textColor: Theme.of(context).primaryColor,
-                                    child: const Text('Descargar PDF'),
-                                  ),
-                                  MaterialButton(
-                                    onPressed: () {
-                                        Navigator.pushNamed( context, 'traducciones_guardadas');
-                                    },
-                                    textColor: Theme.of(context).primaryColor,
-                                    child: const Text('Regresar'),
-                                  ),
-                                ],
-                                
-                              )
-                              );
-                            },
-                            child: const Text('Traducción'),
-                          ),
-                        MaterialButton(
-                            onPressed: (){
-                              showDialog( 
-                                context: context, 
-                                builder: (BuildContext context) => 
-                                AlertDialog(
-                                title: const Text('¿Está seguro en eliminar esta traducción?'),
-                                actions: [
-                                  MaterialButton(
-                                    onPressed: () {
-                                        Navigator.pushNamed( context, 'traducciones_guardadas');
-                                    },
-                                    textColor: Theme.of(context).primaryColor,
-                                    child: const Text('Cancelar'),
-                                  ),
-                                  MaterialButton(
-                                    onPressed: () {
-                                      // print("DOCUMENTO ELIMINDADO: " + document.id);
-                                      FirebaseFirestore.instance.collection('usuarios').doc(document.id).delete(); //GRACIAS A QUE ACCEDEMOS MEDIANTE EL ID DEL DOCUMENTO, LA ELIMINACION ES MAS SENCILLA
-                                        Navigator.pushNamed( context, 'traducciones_guardadas');
-                                    },
-                                    textColor: Theme.of(context).primaryColor,
-                                    child: const Text('Eliminar'),
-                                  ),
-                                ],
-                              )
-                              );                            
-                            },
-                            child: const Text('Eliminar'),
-                          ),                      
-                      ]
-                    ),
-                  )
-                ]
-              );
-            }).toList(),
-          );
-        },
-        
+      stream: _usersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Algo salio mal');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator(color: AppTheme.primary,);
+        }
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+          Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+            return Column(
+              children: <Widget>[
+                tarjeta(data['Titulo'], data['Descripción'], data['Texto_guardado'], document),
+                Divider(color: AppTheme.primary, height: 5, indent: 10, endIndent: 10,),
+              ]
+            );
+          }).toList(),
+        );
+      },
     );
   }
 
-  //APARTADO DONDE SE CREA EL PDF A DESCARGAR
-  Future<void> _crearPDF(String tituloPDF, String textoPdf) async {
-    PdfDocument documentoPDF = PdfDocument();
-    final pagina = documentoPDF.pages.add();
-
-    pagina.graphics.drawString(textoPdf, PdfStandardFont(PdfFontFamily.helvetica, 30));
-    
-
-    List<int> bytes = documentoPDF.save();
-    documentoPDF.dispose();
-
-    guardarMostrarPdf(bytes, tituloPDF);
+  Widget tarjeta(String titulo, String descripcion, String contenido, DocumentSnapshot document){
+    return Card(
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0)
+        )
+      ),
+      child:Container(
+        height: 100,
+        color: Colors.white,
+        child: Expanded(
+          child:Container(
+            alignment: Alignment.topLeft,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: ListTile(
+                    title: Text(titulo),
+                    subtitle: Text(descripcion),
+                  ),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        child: const Text("Ver"),
+                        onPressed: ()
+                        {
+                          _showResult(contenido);
+                        },
+                      ),
+                      const SizedBox(width: 8,),
+                      TextButton(
+                        child: const Text("Descargar"),
+                        onPressed: _descargar,
+                      ),
+                      const SizedBox(width: 8,),
+                      TextButton(
+                        child: const Text("Eliminar"),
+                        onPressed: (){
+                          _popUpEliminarRegistro(document);
+                        },
+                      ),
+                      const SizedBox(width: 8,),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          // flex:8 ,
+        ),
+      ),
+      elevation: 8,
+      margin: const EdgeInsets.all(10),
+    );
+  }
+  
+  void _popUpEliminarRegistro(DocumentSnapshot document) {
+    showDialog(
+      barrierDismissible: false,
+      context: context, 
+      builder: (BuildContext context) => 
+      CustomPopUp(
+        title: 'Eliminar Traducción',
+        buttonText: 'Cancelar',
+        onPressedFunction: (){ Navigator.pop(context); },
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children:[
+            const Text('¿Seguro que desea eliminar la traducción?',
+              textAlign: TextAlign.center, 
+              style: TextStyle(fontSize: AppTheme.size18),
+            ),
+            const Divider(), 
+            TextButton(
+              onPressed: () {
+                FirebaseFirestore.instance.collection('usuarios').doc(document.id).delete(); //GRACIAS A QUE ACCEDEMOS MEDIANTE EL ID DEL DOCUMENTO, LA ELIMINACION ES MAS SENCILLA
+                Navigator.pushNamed( context, 'traducciones_guardadas');
+              },
+              child: const Text('Confirmar',
+              style:  TextStyle(
+                fontSize: AppTheme.size20,
+                color: Colors.red,
+                ),
+              ),
+            ),
+          ],
+        )
+      ),
+    );
   }
 
-  
- // TODO: Hacer widget de POPuP
-  //POPUP QUE APARECERA CUANDO QUIERA ELIMINARSE EL REGISTRO DESEADO
-  Widget popUpEliminarRegistro(BuildContext context) {
-  return AlertDialog(
-    title: const Text('¿Está seguro en eliminar esta traducción?'),
-    actions: [
-      MaterialButton(
-        onPressed: () {
-          final rutaTraduccionesGuardadas = MaterialPageRoute(
-                builder: (context){
-                  return const TraduccionesGuardadasPage();
-                }
-              );
-            Navigator.push( context, rutaTraduccionesGuardadas);
-        },
-        textColor: Theme.of(context).primaryColor,
-        child: const Text('Cancelar'),
+  void _showResult(String text) {
+    showDialog(
+      context: context, 
+      builder: (BuildContext context) => 
+      CustomPopUp(
+        title: 'Texto Guardado',
+        buttonText: 'Ok',
+        onPressedFunction: (){ Navigator.pop(context); },
+        content: Text(text),
       ),
-      MaterialButton(
-        onPressed: () {
-          final rutaTraduccionesGuardadas = MaterialPageRoute(
-                builder: (context){
-                  return const TraduccionesGuardadasPage();
-                }
-              );
-            Navigator.push( context, rutaTraduccionesGuardadas);
-        },
-        textColor: Theme.of(context).primaryColor,
-        child: const Text('Eliminar'),
-      ),
-    ],
-  );
-}
+    );
+  }
 
+  void _descargar(){
+    showDialog(
+      context: context, 
+      builder: (BuildContext context) => 
+      CustomPopUp(
+        title: 'Seleccione una opción',
+        buttonText: 'Cancelar',
+        onPressedFunction: (){Navigator.pop(context);},
+        content: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: (){Navigator.pop(context);}, 
+              child: const Text('Descargar PDF',
+              style: TextStyle(
+                fontSize: AppTheme.size18,
+                color: Colors.black,)
+                )
+              ),
+            const Divider(),
+            TextButton(
+              onPressed: (){}, 
+              child: const Text('Descargar PDF espejo',
+              style: TextStyle(
+                fontSize: AppTheme.size18,
+                color: Colors.black,),
+              ),
+            ),
+            const Divider(), 
+            TextButton(
+              onPressed: (){}, 
+              child: const Text('Descargar .brf',
+              style:  TextStyle(
+                fontSize: AppTheme.size18,
+                color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
